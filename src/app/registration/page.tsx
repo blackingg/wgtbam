67cde77a9ebaa08c06c85c6a2dcaece22a9a5e03
Umoh Samuel Godwin <umohsg@gmail.com>
2024-06-18 22:43:08 +0100

    tweaking mobile reg screen

--- src/app/registration/page.tsx
+++ src/app/registration/page.tsx
@@ -17,7 +17,7 @@ const RegistrationUser = () => {
     />
   ) : (
     <div className="purplebg flex min-h-[100dvh] w-full items-center justify-center px-[5%] text-white">
-      <div className="cscale-90 relative z-10 mt-[55px] flex w-full max-w-[550px] flex-col items-center justify-center rounded-3xl border border-white bg-opacity-[0.15] bg-gradient-to-br from-white/10 to-white/0 px-[15px] pb-[30px] pt-[55px] shadow-md backdrop-blur-sm tablet:px-[30px]">
+      <div className="lg:cscale-90 relative z-10 mt-[55px] flex w-full max-w-[550px] flex-col items-center justify-center rounded-3xl pb-[30px] pt-[55px] tablet:px-[30px] lg:border lg:border-white lg:bg-opacity-[0.15] lg:bg-gradient-to-br lg:from-white/10 lg:to-white/0 lg:shadow-md lg:backdrop-blur-sm">
         <div className="absolute left-[50%] top-[0%] z-[999] w-full max-w-[150px] translate-x-[-50%] translate-y-[-50%] tablet:max-w-[200px]">
           <Image
             src="/Images/logo.png"
