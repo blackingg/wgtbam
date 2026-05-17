7ae0064d3f2efccdc4c3565353b2555de995e915
blackingg <odetundemubarak04@gmail.com>
2026-05-17 15:27:58 +0100

    changed logo

--- src/components/common/MillionareLogo.tsx
+++ src/components/common/MillionareLogo.tsx
@@ -4,13 +4,11 @@ export const MillionareLogo = () => {
   return (
     <div className=" relative w-full h-full ">
       <Image
-        src="/Images/logo2.png"
+        src="/Images/logo.png"
         alt="WGTBAM Logo"
         width={250}
         height={250}
         priority
-        placeholder="blur"
-        blurDataURL="/Images/logo2.png"
         quality={100}
         sizes="150px, 250px, 350px"
         className=" object-cover bg-no-repeat max-w-[150px] tablet:max-w-[250px] max-h-[150px] tablet:max-h-[250px] w-full h-full flex justify-center items-center mx-auto cursor-not-allowed"
